d9dfdcfe4c83d0319987cf8598bd864b660e0542
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-23 16:39:07 -0300

    :recycle: refactor :criando classe pokemon para facilitar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 const convertPokemonHtml = (pokemon) => {
     return `   <li class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">${pokemon.number}</span>
     <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type">${ type }</li>`).join('')}
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+        <img src=${pokemon.photo} 
            alt="${pokemon.name} image">
     </div>
 </li>`
