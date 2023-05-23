78c41b6b37e862bff14ed87bc7cae296f4b69233
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-23 15:28:45 -0300

    :sparkles: listando nome dos pokemons da lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
-const offset = 0
-const limit = 10
+const offset = 0;
+const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
-fetch(url).then((response) => {
-    console.log(response)
-})
+const convertPokemonHtml = (pokemon) => {
+    return `   <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+           alt="${pokemon.name} image">
+    </div>
+</li>`
+ 
+}
 
-const x = 10 + 10;
-console.log(x)
+const pokemonList = document.getElementById('pokemonList');
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokes) => {
+        pokes.map((pokemon) => pokemonList.innerHTML +=(convertPokemonHtml(pokemon)))
+    })
+    .catch((error) => console.error(error))
